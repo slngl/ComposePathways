ae2b8577d4dafc91b29a379b7ffdd6b42297cb3f
slngl <dselin26@gmail.com>
2025-03-26 02:06:05 +0300

    added onBoardingScreen. explained remember keyword and state hoisting

--- BasicsCodelabOnboardingList/app/src/main/java/com/slngl/basicscodelabonboardinglist/MainActivity.kt
+++ BasicsCodelabOnboardingList/app/src/main/java/com/slngl/basicscodelabonboardinglist/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +43,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+
+    // shouldShowOnboarding is using a by keyword instead of the =.
+    // This is a property delegate that saves you from typing .value every time.
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false }, modifier)
+    } else {
+        Greetings(modifier)
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
 /**
  * Modifiers tell a UI element how to lay out, display, or behave within its parent layout.
  * As a best practice, your function should include a Modifier parameter that is
@@ -46,22 +86,27 @@ class MainActivity : ComponentActivity() {
  * layout instructions and behaviors from outside of your composable function.
  * */
 @Composable
-fun MyApp(
+fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000){ "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+// LazyColumn is a composable that arranges its children in a scrollable column.
+// This is the same as using a LazyColumn inside a Column composable
+//    LazyColumn renders only the visible items on screen, allowing performance gains when rendering a big list.
+//    Note: LazyColumn and LazyRow are equivalent to RecyclerView in Android Views.
+
+    LazyColumn (modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
 }
 
-/*
+/**
  * When a composable function is called multiple times, each call creates a separate instance of the UI element.
  * If state is created within the composable using the remember API, that state is local to that specific instance.
  * Changes to the state in one instance will not affect other instances.
- */
+ * */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var expanded by remember { mutableStateOf(false) }
@@ -94,6 +139,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BasicsCodelabOnboardingListTheme {
-        MyApp()
+        Greetings()
     }
 }
