f55fd558c58f4a46f11e4c68c2845b2c980eb69b
slngl <dselin26@gmail.com>
2025-03-26 00:33:47 +0300

    what is state in compose?

--- BasicsCodelabOnboardingList/app/src/main/java/com/slngl/basicscodelabonboardinglist/MainActivity.kt
+++ BasicsCodelabOnboardingList/app/src/main/java/com/slngl/basicscodelabonboardinglist/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.slngl.basicscodelabonboardinglist.ui.theme.BasicsCodelabOnboardingListTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicsCodelabOnboardingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    MyApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+/**
+ * Modifiers tell a UI element how to lay out, display, or behave within its parent layout.
+ * As a best practice, your function should include a Modifier parameter that is
+ * assigned an empty Modifier by default. Forward this modifier to the first composable
+ * you call inside your function. This way, the calling site can adapt
+ * layout instructions and behaviors from outside of your composable function.
+ * */
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+/*
+ * When a composable function is called multiple times, each call creates a separate instance of the UI element.
+ * If state is created within the composable using the remember API, that state is local to that specific instance.
+ * Changes to the state in one instance will not affect other instances.
+ */
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(extraPadding)
+            ) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded },
+                modifier = Modifier.padding(vertical = 4.dp)
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabOnboardingListTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
